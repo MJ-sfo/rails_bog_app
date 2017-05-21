53fa45c5bde8352f33157e407e44bfe376d48da5
MJ-sfo <michaeljlaird@yahoo.com>
2017-05-21 13:05:15 -0700

    new form adds swamp creature to web

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -6,4 +6,17 @@ class CreaturesController < ApplicationController
     # render the index view (it has access to instance variable)
     # remember the default behavior is to render :index
   end
+
+  # show the new creature form
+  def new
+    # remember the default behavior is to render :new
+  end
+
+  def create
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature = Creature.new(creature_params)
+    if creature.save
+      redirect_to creature_params
+    end
+  end    #def create
 end
